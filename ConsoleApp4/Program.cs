787b9ff07f125e72c3f8575df22af311df44c047
msr <msr@DESKTOP-N4TFJI9>
2024-06-12 09:59:39 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = default;
+            int a = default;
+            float b = default;
+            double c = default;
+            char d = default;
+            bool e = default;
+            decimal f = default;
+            byte g = default;
+            short h = default;
+            long i = default;
+            object j = default;
+            Console.WriteLine("String default value :" + x);
+            Console.WriteLine("Int default value:"+a);
+            Console.WriteLine("Float  default value :" + b);
+           Console.WriteLine("Double default value :" + c);
+            Console.WriteLine("Char  default value :" + d);
+            Console.WriteLine("Boolean  default value :" + e);
+            Console.WriteLine("Byte defaulst value :"+f);
+            Console.WriteLine("Short default value :" + h);
+            Console.WriteLine("Long  default value :" + i);
+            Console.WriteLine("Object default value :" + j);
+        }
+    }
+}
